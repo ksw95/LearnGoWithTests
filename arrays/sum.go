38d954d4dabf020ef175a46468513336d88e5b39
ksw95 <kshaowei95@gmail.com>
2024-08-11 17:53:04 +0800

    Added function to sum up all tails variables in multiple slices and arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,8 +1,23 @@
 package main
 
-func Sum(array []int) (sum int) {
-	for _, number := range array {
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
 		sum += number
 	}
 	return
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, numbersInSlices := range slices {
+		if len(numbersInSlices) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbersInSlices[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -12,4 +15,28 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(got, want []int, t testing.TB) {
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("get the sum of single or multiple slices", func(t *testing.T) {
+		got := SumAllTails([]int{2, 6}, []int{3, 4, 6})
+		want := []int{6, 10}
+
+		checkSums(got, want, t)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+
+		checkSums(got, want, t)
+	})
+
 }
